0c3eeb66c8ad35a1d69f41388299c3bced04cc86
Daniel Sampaio Pereira <daniel.pereira.vr@gmail.com>
2025-05-21 21:52:26 +0100

    upedate/FeatureCard

--- pitstop-pro/src/components/Features.tsx
+++ pitstop-pro/src/components/Features.tsx
@@ -6,7 +6,7 @@ function Features() {
 		<section className='bg-background text-text py-16 px-6'>
 			<div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3 gap-8 text-center'>
 				<FeatureCard Icon={Wrench} title='Organized Maintenance' description='Easily track and schedule each vehicle service.' />
-				<FeatureCard Icon={CurrencyEur} title='Expense Tracking' description='Know exactly how much you vehicle is costing you on maintenance, repairs and insurance.' />
+				<FeatureCard Icon={CurrencyEur} title='Expense Tracking - coming soon' description='Know exactly how much you vehicle is costing you on maintenance, repairs and insurance. ' />
 				<FeatureCard Icon={ListBullets} title='List your Next Events' description='Never forget your inspections, insurance payments or IUC again.' />
 			</div>
 		</section>
