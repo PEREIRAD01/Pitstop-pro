4b5c0a05b6545830798be86ac344cb38550f2c93
Daniel Sampaio Pereira <daniel.pereira.vr@gmail.com>
2025-05-25 21:49:29 +0100

    Configure GitHub Pages deployment

--- pitstop-pro/vite.config.ts
+++ pitstop-pro/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+	base: '/Pitstop-pro/',
 	plugins: [react()],
 	server: {
 		port: 3000,
